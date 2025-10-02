7e78273fcf883a473e396c2bda82fbf920b3a850
Jachym Richtrmoc <76998554+hootmole@users.noreply.github.com>
2025-10-02 14:54:32 +0200

    Update main_line_follower.py

--- main_line_follower.py
+++ main_line_follower.py
@@ -5,15 +5,15 @@ import time, machine
 from lib.robot_consts import Button, Port, Sensor, Light
 
 # TODO Change these constants
-dt = 0.01      # Regulation period 0.010 s <=> 100 Hz
-MOTOR_BASE_POWER = 50           # 0-100 %
+REGULATION_PERIOD_MS = 10       # Regulation period 0.010 s <=> 100 Hz
+MOTOR_BASE_POWER = 85           # 0-100 %
 LIGHT_SETPOINT = 21             # 
-Kp, Ki, Kd = 5, 0, 0            # PID constants
+Kp, Ki, Kd = 3, 2.5, 0.25           # PID constants
 
 
 # If the program is run from the menu, access global robot variable
 # If it is run independently, robot is initialized
-independent_run = False
+sindependent_run = False
 global robot
 try :
     robot
@@ -22,6 +22,7 @@ except :
     independent_run = True
     
 
+# Initialize motors on ports M2 and M3
 robot.init_motor(Port.M1)   #left motor
 robot.init_motor(Port.M2)	#right motor
 
@@ -34,43 +35,66 @@ error = 0
 last_error, integral, derivative = 0, 0, 0
 output, motor_pwr = 0, 0
 
+#output_min = -100
+#output_max = 100
+#integral_min = -10
+#integral_max = 80
+dt = 0.01
 
-#while not robot.sensors.touch[Port.S2]:
+
+#while not robot.sensors.touch[Port.S2].pressed():
+#    time.sleep_ms(300)
+
+
+#while not robot.sensors.touch[Port]:
 #    time.sleep(0.1)
 time.sleep_ms(2000)
 
     
+# Follower regulation loop
 while True:
+    # Read light intensity
     light_intensity = robot.sensors.light[Port.S1].reflection() 
+
+    # TODO - PID regulator
+    # 1. calculate error value e from light setpoint and measured ligth intensity
+    # e =
+
+    # 2. calculate motor_pwr using error variables (e, e_sum, e_prev) and PID constants (Kp, Ki, Kd)
+    # motor_pwr = 
+
+    # 3. save previous error for the derivative part
+    # e_prev = 
+
+    # 4. Save sum of errors for the integral part
+    # e_sum =
     
     error = LIGHT_SETPOINT - light_intensity
-    
     integral = integral +  error * dt
+    #if(integral > integral_max):integral = integral_max
+    #if(integral < integral_min):integral = integral_min
     derivative = (error - last_error) / dt
-    
     output = Kp * error + Ki * integral + Kd * derivative
-    
     last_error = error
-        
+    
+    #if (output < output_min): output = output_min
+    #if (output > output_max): output = output_max
+    
+    
 
-    # output /= 2.0 # divide the output between 2 motors
-    motor_power = MOTOR_BASE_POWER# - abs(output)
+
+    # Set motor power
     robot.motors[Port.M1].set_power(MOTOR_BASE_POWER + output)
     robot.motors[Port.M2].set_power(MOTOR_BASE_POWER - output)
 
-
-    time.sleep_ms(dt * 1000)
+    # Do nothing
+    time.sleep_ms(REGULATION_PERIOD_MS)
+    print("ss")
+    # Exit program if left cube button is pressed
     buttons = robot.buttons.pressed()
     if buttons[Button.LEFT]:
         break
-
-
+    
 # Reset back to menu
 if independent_run:
     machine.reset()
-    
-    
-    
-    
-    
-    
