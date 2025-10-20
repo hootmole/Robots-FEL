2a42014890511dc34b9e03603bf69f5c430d9c51
Jachym Richtrmoc <76998554+hootmole@users.noreply.github.com>
2025-10-20 14:40:53 +0200

    Adjust motor power and sensor initialization

--- go_around.py
+++ go_around.py
@@ -32,7 +32,7 @@ def go_around(phase):
             if phase:
                 robot.motors[Port.M1].set_power(-15)
                 robot.motors[Port.M2].set_power(15)
-                time.sleep_ms(500)
+                time.sleep_ms(300)
                 
             else:
                 robot.motors[Port.M1].set_power(15)
@@ -66,9 +66,8 @@ robot.init_motor(Port.M1)   #left motor
 robot.init_motor(Port.M2)	#right motor
 
 robot.init_sensor(sensor_type=Sensor.OC_COLOR, port=Port.S1)
-#robot.init_sensor(sensor_type=Sensor.OC_LASER, port=Port.S2)
-robot.init_sensor(sensor_type=Sensor.NXT_ULTRASONIC)
-
+robot.init_sensor(sensor_type=Sensor.OC_LASER, port=Port.S2)
+#robot.init_sensor(sensor_type=Sensor.NXT_ULTRASONIC)
 
 
 # Regulator variables
@@ -79,7 +78,7 @@ output, motor_pwr = 0, 0
 
 dt = 0.01
 dist = 0
-stopping_dist = 100
+stopping_dist = 80
 
 phase = 1
 
@@ -89,15 +88,16 @@ motor_power = 0
 while True:
     # Read light intensity
     light_intensity = robot.sensors.light[Port.S1].reflection()
-    #dist = robot.sensors.laser[Port.S2].distance()
-    dist = robot.sensors.ultra_nxt.distance() * 10
+    dist = robot.sensors.laser[Port.S2].distance()
+    #dist = robot.sensors.ultra_nxt.distance() * 10
     
-    if (dist < 5 * stopping_dist) and not ((dist - stopping_dist) < 10):
-        motor_power = MOTOR_BASE_POWER * (dist - stopping_dist) / (4 * stopping_dist)
+    #if (dist < 5 * stopping_dist) and not ((dist - stopping_dist) < 10):
+    #    motor_power = MOTOR_BASE_POWER * (dist - stopping_dist) / (4 * stopping_dist)
         
-    else: motor_power = MOTOR_BASE_POWER
+    #else: motor_power = MOTOR_BASE_POWER
+    motor_power = MOTOR_BASE_POWER
     
-    if (dist - stopping_dist) < 10:
+    if (dist - stopping_dist) < 20:
         go_around(phase)
         last_error = 0
         integral = 0
